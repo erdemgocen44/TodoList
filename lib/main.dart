0f8553e1c9be9627846e54e39f53bf6cec4b8370
Erdem <erdemgocen44@gmail.com>
2022-09-10 20:03:55 +0200

    todoList starting

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoList());
+}
+
+class TodoList extends StatelessWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
