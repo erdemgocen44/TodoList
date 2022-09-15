f674722b089776d7269f6fd675cab94fd2ac7712
Erdem <erdemgocen44@gmail.com>
2022-09-15 20:18:43 +0200

    classeur d'accord

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await TaskerPreference.init();
-
-  runApp(const TodoList());
+void main() {
+  runApp(const MyApp());
 }
 
 class TodoList extends StatelessWidget {
   const TodoList({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>ChangeNotifierProvider(
-    create: (context) => ThemProvider(),
-    builder:(context,){
-      final themProvider=Provider.of<ThemProvider>(context);
-      return MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Home(),
     );
   }
 }
-    }
-  )
-    
