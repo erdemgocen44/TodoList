da2d9ce1449c277805e7be66b3dcb9641aea9a97
Erdem <erdemgocen44@gmail.com>
2022-09-20 20:59:12 +0200

    classeur d'accord

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/constants/color.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,7 +8,16 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
+        backgroundColor: tdBGColor,
+        title: Row(
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),Container(child: ,)
+          ],
+        ),
       ),
       body: Container(
         child: const Text('This is home screen'),
