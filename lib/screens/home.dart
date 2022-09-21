99dd8c35bc62d55f0de2b22041f705412cadedeb
Erdem <erdemgocen44@gmail.com>
2022-09-21 21:39:06 +0200

    textfield ok

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,25 +13,32 @@ class Home extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
           children: [
-            Container(padding: EdgeInsets.symmetric(),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: tdBlack,
-                    size: 20,
-                  ),
-                  prefixIconConstraints:
-                      BoxConstraints(maxHeight: 20, minWidth: 25),
-                border: InputBorder.none,hintText: 'Search',hintStyle:TextStyle(color:tdGrey ),
-              ),
-            ),
+            searchBox(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
         ),
       ),
     );
