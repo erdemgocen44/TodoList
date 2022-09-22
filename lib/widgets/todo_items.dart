5cac82369202c42bcc8642f28998be35c4d1fdc2
Erdem <erdemgocen44@gmail.com>
2022-09-22 19:39:42 +0200

    todo items starting

--- lib/widgets/todo_items.dart
+++ lib/widgets/todo_items.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class TodoItems extends StatefulWidget {
+  const TodoItems({Key? key}) : super(key: key);
+
+  @override
+  State<TodoItems> createState() => _TodoItemsState();
+}
+
+class _TodoItemsState extends State<TodoItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('ToDo Items'),
+    );
+  }
+}
