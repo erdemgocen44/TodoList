8a721ba4f70fc9784c76ad6fa7ffdc46c35388e2
Erdem <erdemgocen44@gmail.com>
2022-10-28 22:02:18 +0200

    todo class starting

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,5 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class ToDo {
   String? id;
   String? toDoText;
   bool isDone;
+  ToDo({
+    this.id,
+    this.toDoText,
+    required this.isDone,
+  });
 }
