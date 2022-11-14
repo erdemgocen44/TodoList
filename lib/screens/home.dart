a5f1fde4f5cde09a0ada97e351fe80e2ef7137cb
Erdem <erdemgocen44@gmail.com>
2022-11-14 17:17:23 +0100

    hata giderilemedi

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -49,15 +49,24 @@ class Home extends StatelessWidget {
               children: [
                 Expanded(
                   child: Container(
-                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       boxShadow: const [
                         BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0.0, 0.0),
-                            blurRadius: 10.0,
-                            spreadRadius: 0.0),
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
                       ],
                       borderRadius: BorderRadius.circular(10),
                     ),
